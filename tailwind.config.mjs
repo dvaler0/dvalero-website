4379a699d480f03a60b54b37ff3a723afaa6f660
David Valero <valeroterrondavid@gmail.com>
2026-01-05 18:32:56 +0100

    Remove animation delay

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,23 +10,7 @@ export default {
 		'./node_modules/flowbite/**/*.js'
 	],
 	theme: {
-		extend: {
-			keyframes: {
-				'fade-in-down': {
-					'0%': {
-						opacity: '0',
-						transform: 'translateY(-20px)',
-					},
-					'100%': {
-						opacity: '1',
-						transform: 'translateY(0)',
-					},
-				},
-			},
-			animation: {
-				'fade-in-down': 'fade-in-down 0.8s ease-out',
-			},
-		},
+		extend: {},
 	},
 	plugins: [
 		flowbitePlugin,
