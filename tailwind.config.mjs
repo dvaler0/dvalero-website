085e0e6f0d8054c6ad59c55c35abd3e2a9a408c9
David Valero <valeroterrondavid@gmail.com>
2026-01-08 18:27:29 +0100

    ⚙️ Remove flowbite dependency and related configurations from package.json, pnpm-lock.yaml, and Tailwind CSS setup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,13 @@
-import { createRequire } from 'module';
 import animations from '@midudev/tailwind-animations';
-
-const require = createRequire(import.meta.url);
-const flowbitePlugin = require('flowbite/plugin');
 
 export default {
 	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js'
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
 	],
 	theme: {
 		extend: {},
 	},
 	plugins: [
-		flowbitePlugin,
 		animations
 	],
 }
